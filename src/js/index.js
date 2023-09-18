3985fbcdd876db862dcf6ae7e22301da1084fb63
Akkhazriel <akkhazriel@gmail.com>
2023-09-18 13:01:38 +0300

    add section reviews

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
 import aduptiveMenu from "./modules/menu-button";
+function cb() {
+    let val = document.querySelector('input[name="star"]:checked').value
+    
+    switch(val) {
+		case '1':
+			text = arr[0];
+			break;
+		case '2':
+			text = arr[1];
+			break;
+		case '3':
+			text = arr[2];
+			break;
+		case '4':
+			text = arr[3];
+			break;
+		case '5':
+			text = arr[4];
+			break;
+    }
+};
+
+document.forms[0].addEventListener('change', cb);
